3e3a52cccd7db59093c8030d2f29937d7419b142
5342626 <5342626@gmail.com>
2015-09-13 19:16:32 +0900

    추가...

--- spec/helpers/jasmine_examples/SpecHelper.js
+++ spec/helpers/jasmine_examples/SpecHelper.js
@@ -3,3 +3,6 @@ Test = require('../../../lib/jasmine_examples/Test');
 World = require('../../../lib/ttl/World');
 Actor = require('../../../lib/ttl/Actor');
 Random = require('../../../lib/ttl/Random');
+SpawnAction = require('../../../lib/ttl/SpawnAction')
+WorldMoveAction = require('../../../lib/ttl/WorldMoveAction')
+WorldSpawn = require('../../../lib/ttl/WorldSpawn')
