0673913c272cb09fb8d79ba9c2f3f695e4619b85
5342626 <5342626@gmail.com>
2015-09-18 00:37:18 +0900

    크으...

--- spec/ttl/MoveSpec.js
+++ spec/ttl/MoveSpec.js
@@ -1,12 +1,10 @@
 'use strict';
 describe('Move', function() {
-    var world;
-
     beforeEach(function() {
-        world = new World();
     });
 
     function testBasicMovement(aix, aiy, dix, diy) {
+        var world = new World();
         var actor = new Actor();
         var action = new WorldMoveAction(dix, diy);
         var stepCost = 3;
@@ -34,6 +32,7 @@ describe('Move', function() {
         expect(cell.isEmpty()).toBeTruthy();
         expect(world.getStep()).toEqual(0);
         expect(spawnAction.execute.calls.count()).toBe(0);
+        expect(action.canBeAchieved()).not.toBeDefined(); // 액션이 달성될 수 있는가?
 
         /***/world.nextStep();/***/
 
@@ -49,7 +48,7 @@ describe('Move', function() {
 
         expect(action.isCostMet()).toBeFalsy(); // 액션 비용이 만족되었는가?
         expect(action.isDone()).toBeFalsy(); // 액션이 수행됐는가?
-        expect(action.canBeAchieved()).toBeTruthy(); // 액션이 달성될 수 있는가?
+        expect(action.canBeAchieved()).not.toBeDefined(); // 액션이 달성될 수 있는가?
 
         /***/world.nextStep();/***/
 
@@ -60,11 +59,17 @@ describe('Move', function() {
         expect(sc.nextStep.calls.count()).toBe(2);
         expect(action.isCostMet()).toBeFalsy(); // 액션 비용이 만족되었는가?
         expect(action.isDone()).toBeFalsy(); // 액션이 수행됐는가?
-        expect(action.canBeAchieved()).toBeTruthy(); // 액션이 달성될 수 있는가?
+        expect(action.canBeAchieved()).not.toBeDefined(); // 액션이 달성될 수 있는가?
         expect(actor.getIx()).toBe(aix);
         expect(actor.getIy()).toBe(aiy);
 
+        spyOn(actor, 'move').and.callThrough();
+        spyOn(cell, 'move').and.callThrough();
+
         /***/world.nextStep();/***/
+
+        expect(actor.move.calls.count()).toBe(1);
+        expect(cell.move.calls.count()).toBe(1);
 
         expect(world.getStep()).toEqual(3);
         expect(spawnAction.execute.calls.count()).toBe(1); // 끝난 액션은 더 실행되면 안된다.
@@ -88,29 +93,35 @@ describe('Move', function() {
         var aix = 5,
             aiy = 8;
         testBasicMovement(aix, aiy, 1, 0); // 우로 이동
-        //testBasicMovement(aix, aiy, -1, 0); // 좌로 이동
-        //testBasicMovement(aix, aiy, 0, 1); // 위로 이동
-        //testBasicMovement(aix, aiy, 0, -1); // 아래로 이동
+        testBasicMovement(aix, aiy, -1, 0); // 좌로 이동
+        testBasicMovement(aix, aiy, 0, 1); // 위로 이동
+        testBasicMovement(aix, aiy, 0, -1); // 아래로 이동
     });
 
-    xit('액터 하나 우로 이동 (이동 불가 확인)', function() {
-        var a = new Actor();
-        var dix = 1,
+    it('원점에서 액터 하나 좌로 이동 불가능함을 확인 (경계 조건)', function() {
+        var world = new World();
+        var actor = new Actor();
+        // (0,0)에서 왼쪽으로 이동하는 것은 불가능하다.
+        var aix = 0,
+            aiy = 0,
+            dix = -1,
             diy = 0;
         var action = new WorldMoveAction(dix, diy);
-        expect(a.getIntentCount()).toEqual(0);
-        a.appendIntent(action);
-        expect(a.getIntentCount()).toEqual(1);
-        expect(world.spawn(a1, 0, 0)).toBeTruthy();
+        expect(actor.getIntentCount()).toEqual(0);
+        actor.appendIntent(action);
+        expect(actor.getIntentCount()).toEqual(1);
+        var spawnAction = new WorldSpawn(actor, aix, aiy);
+        expect(world.appendIntent(spawnAction)).toBeTruthy();
         // 월드 스테핑을 최소 한번 하기 전에는 달성 가능 여부를 확인할 수 없다.
         expect(action.canBeAchieved()).not.toBeDefined();
         world.nextStep();
         // 경계 밖으로 이동하는 것은 불가능하다.
         expect(action.canBeAchieved()).toBeFalsy();
-        expect(a.getIntentCount()).toEqual(0);
+        expect(actor.getIntentCount()).toEqual(0);
     });
 
     function SpawnActorMoveDelta(aix, aiy, dix, diy) {
+        var world = new World();
         var a = new Actor();
         var moveAction = new WorldMoveAction(dix, diy);
         expect(a.appendIntent(moveAction)).toBeTruthy();
@@ -126,7 +137,8 @@ describe('Move', function() {
     /*
         [A][B][ ] -> [ ][A][B]
     */
-    xit('액터 둘이 인접한 상태로 함께 오른쪽 방향으로 이동하기', function() {
+    it('액터 둘이 인접한 상태로 함께 오른쪽 방향으로 이동하기', function() {
+        var world = new World();
         var set1 = SpawnActorMoveDelta(0, 0, 1, 0);
         var set2 = SpawnActorMoveDelta(1, 0, 1, 0);
         world.nextStep();
@@ -135,7 +147,8 @@ describe('Move', function() {
         expect(world.getCell(2, 0).getOwner()).toEqual(set2.actor);
     });
 
-    xit('같은 셀에 두 액터가 동시에 들어가려고 할 때 먼저 들어가길 요청한 액터 하나만 들어감', function() {
+    it('같은 셀에 두 액터가 동시에 들어가려고 할 때 먼저 들어가길 요청한 액터 하나만 들어감', function() {
+        var world = new World();
         var set1 = SpawnActorMoveDelta(0, 0, 1, 0);
         var set2 = SpawnActorMoveDelta(2, 0, -1, 0);
         world.nextStep();
@@ -144,7 +157,8 @@ describe('Move', function() {
         expect(world.getCell(2, 0).getOwner()).toEqual(set2.actor);
     });
 
-    xit('텔-레-포-트', function() {
+    it('텔-레-포-트', function() {
+        var world = new World();
         var a = new Actor();
         var aix = 0, aiy = 0;
         var bix = 10, biy = 20;
--- spec/ttl/WorldSpec.js
+++ spec/ttl/WorldSpec.js
@@ -1,17 +1,17 @@
-describe('World', function () {
+describe('World', function() {
     var world;
 
-    beforeEach(function () {
+    beforeEach(function() {
         world = new World();
     });
 
-    it('스테핑을 해 보자', function () {
+    it('스테핑을 해 보자', function() {
         expect(world.getStep()).toEqual(0);
         expect(world.nextStep()).toBeTruthy();
         expect(world.getStep()).toEqual(1);
     });
 
-    it('월드 셀 개수는 가로 9개, 세로 160개로 한다. 그리고 모두 비어있다.', function () {
+    it('월드 셀 개수는 가로 9개, 세로 160개로 한다. 그리고 모두 비어있다.', function() {
         expect(world.getCellCountX()).toEqual(9);
         expect(world.getCellCountY()).toEqual(160);
         for (var iy = 0; iy < 160; ++iy) {
@@ -19,5 +19,12 @@ describe('World', function () {
                 expect(world.getCell(ix, iy).isEmpty()).toBeTruthy();
             }
         }
+    });
+
+    it('범위 밖 셀 조회 시 null 반환', function() {
+        expect(world.getCell(-1, 0)).toBeNull();
+        expect(world.getCell(999, 0)).toBeNull();
+        expect(world.getCell(5, -10)).toBeNull();
+        expect(world.getCell(5, 160)).toBeNull();
     });
 });
