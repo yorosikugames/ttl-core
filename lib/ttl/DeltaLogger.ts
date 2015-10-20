b2e867751c03e548031aba3e2078f3c6002114be
Julian Ken <faults904@gmail.com>
2015-10-21 01:58:49 +0900

    TypeScript로 포팅 중

---
+++ lib/ttl/DeltaLogger.ts
@@ -0,0 +1,24 @@
+﻿class DeltaLogger {
+
+    deltaList: Array<any>;
+
+    constructor() {
+        this.deltaList = new Array<any>();
+    }
+
+    enqueue(delta: any) {
+        this.deltaList.push(delta);
+    }
+
+    pop() {
+        if (this.deltaList.length > 0) {
+            var r = this.deltaList[0];
+            this.deltaList.splice(0, 1);
+            return r;
+        } else {
+            return null;
+        }
+    }
+}
+
+export = DeltaLogger;
--- lib/ttl/PoisonMod.ts
+++ lib/ttl/PoisonMod.ts
@@ -1,18 +1,23 @@
-var PoisonMod = (function () {
-    function PoisonMod(damagePerStep, duration) {
+﻿class PoisonMod {
+
+    damagePerStep: number;
+    duration: number;
+    age: number;
+
+    constructor(damagePerStep: number, duration: number) {
         this.damagePerStep = damagePerStep;
         this.duration = duration;
         this.age = 0;
     }
-    PoisonMod.prototype.execute = function (actor) {
+
+    execute(actor: any) {
         if (this.age < this.duration) {
             // 독 데미지는 스스로 입히는 것이다?
             actor.applyDamage(actor, this.damagePerStep);
         }
         this.age++;
         return this.age < this.duration;
-    };
-    return PoisonMod;
-})();
-module.exports = PoisonMod;
-//# sourceMappingURL=PoisonMod.js.map
+    }
+}
+
+export = PoisonMod;
